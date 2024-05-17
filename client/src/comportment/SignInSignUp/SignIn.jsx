c475eaa668782ab4e6debf20229e265ec9bff87e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-17 23:24:03 +0530

    Update SignIn.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportment/SignInSignUp/SignIn.jsx
+++ client/src/comportment/SignInSignUp/SignIn.jsx
@@ -33,7 +33,7 @@ const SignIn = () => {
 
   return (
     <div>
-        <div className={`py-8 lg:px-20 px-8 lg:h-screen h-auto lg:w-full ${!Darkmode ? 'light-mode duration-500 bg-gray-200' : 'dark-mode duration-500 bg-[#2c3646]'}`}>
+        <div className={`py-8 lg:px-20 px-8 lg:h-screen h-auto lg:w-full ${!Darkmode ? 'light-mode duration-500 bg-gray-200' : 'dark-mode duration-500 bg-[#1e293b]'}`}>
             {
                 !Darkmode ? 
                     <p onClick={() => headleDarkMode()}>LightMode</p>
@@ -42,21 +42,20 @@ const SignIn = () => {
             }
             
             <div className="">
-                <div className="lg:grid grid-cols-3 gap-2 lg:py-16">
+                <div className="lg:grid grid-cols-3 gap-2 lg:py-16"> 
                     <div className=""></div>
 
-                    <div className="bg-white lg:mx-20 my-16 py-8 rounded shadow-md">
+                    <div className={`lg:mx-20 my-16 py-8 rounded shadow-md ${!Darkmode ? 'light-mode duration-500 bg-white' : 'dark-mode duration-500 bg-[#2c3646]'}`}>
                         <div className="text-center my-8">
                             <p className="text-blue-500 text-4xl pt-8"><MyIcons name='school' size='large'></MyIcons></p>
                             <h1 className="text-2xl">Welcome Back</h1>
                             <p className="lg:text-3xl my-2">UMS</p>
                         </div>
-                        <hr />
                         <div className="mx-8 ">
                             <form onSubmit={headleSignIn}>
                                 <div className="my-8">
                                     <label htmlFor="" className='my-2 lg:text-2xl'>Email : </label>
-                                    <input type="email" name="" id="" className="my-2 text-xl h-16 w-full rounded bg-gray-200 pl-2 mr-4" required placeholder='Enter Email Address'
+                                    <input type="email" name="" id="" className={`my-2 text-xl h-16 w-full rounded pl-2 mr-4  ${!Darkmode ? 'bg-gray-200 light-mode duration-500' : 'dark-mode duration-500 bg-[#1e293b]'}`} required placeholder='Enter Email Address'
                                     onChange={e => SetSignInData({...SignInData, email:e.target.value})}/>
                                 </div>
                                 <div className="my-8">
@@ -72,7 +71,6 @@ const SignIn = () => {
                             <div className="my-1 mb-8">
                                 <span><Link><span className='text-blue-600'>Forget Password</span></Link></span>
                             </div>
-                            <hr />
                             <div className="">
                                 <p>Don't have an Account ? <span className='text-blue-600 cursor-pointer'>SignUp</span></p>
                             </div>
