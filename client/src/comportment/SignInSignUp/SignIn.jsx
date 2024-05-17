ebf7f49ffef7af22c2cc4f8653e341fc34f3ca2f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-17 22:44:54 +0530

    Update SignIn.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportment/SignInSignUp/SignIn.jsx
+++ client/src/comportment/SignInSignUp/SignIn.jsx
@@ -9,7 +9,13 @@ const SignIn = () => {
   return (
     <div>
         <div className="py-8 lg:px-20 px-8 bg-gray-200 lg:h-screen h-auto w-full">
-
+            <div className="bg-white mx-20">
+                <div className="lg:grid grid-cols-2 gap-2">
+                    <div className="">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
