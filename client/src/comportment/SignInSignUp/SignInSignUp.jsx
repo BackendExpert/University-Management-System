7e99167f20e1bd7f1d28ede75b92ef3d99410af6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-17 23:36:15 +0530

    Update SignInSignUp.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+anupa1998@users.noreply.github.com>

--- client/src/comportment/SignInSignUp/SignInSignUp.jsx
+++ client/src/comportment/SignInSignUp/SignInSignUp.jsx
@@ -18,7 +18,7 @@ const SignInSignUp = () => {
     }
 
     // headle button values
-    const [buttonValue, SetButtonValue] = useState(0)
+    const [buttonValue, SetButtonValue] = useState("SignIn")
     const HeadleButtonClick = (clickValue) => {
         SetButtonValue(clickValue)   
     }
@@ -35,6 +35,12 @@ const SignInSignUp = () => {
         
         // send data
     }
+
+    const [SignUpData, SetSignUpData] = useState({
+        username: '',
+        SignUpemail: '',
+        SignUppassword: ''
+    })
 
 
   return (
@@ -58,27 +64,82 @@ const SignInSignUp = () => {
                             <p className="lg:text-3xl my-2">UMS</p>
                         </div>
                         <div className="mx-8 ">
-                            <form onSubmit={headleSignIn}>
-                                <div className="my-8">
-                                    <label htmlFor="" className='my-2 lg:text-2xl'>Email : </label>
-                                    <input type="email" name="" id="" className={`my-2 text-xl h-16 w-full rounded pl-2 mr-4  ${!Darkmode ? 'bg-gray-200 light-mode duration-500' : 'dark-mode duration-500 bg-[#1e293b]'}`} required placeholder='Enter Email Address'
-                                    onChange={e => SetSignInData({...SignInData, email:e.target.value})}/>
-                                </div>
-                                <div className="my-8">
-                                    <label htmlFor="" className='my-2 lg:text-2xl'>Password : </label>
-                                    <input type="password" name="" id="" className={`my-2 text-xl h-16 w-full rounded pl-2 mr-4  ${!Darkmode ? 'bg-gray-200 light-mode duration-500' : 'dark-mode duration-500 bg-[#1e293b]'}`} required placeholder='Enter Password'
-                                    onChange={e => SetSignInData({...SignInData, password:e.target.value})}/>
-                                </div>
-                                <div className="">
-                                    <button type='submit' className='w-full h-14 bg-blue-500 rounded text-white duration-500 hover:bg-blue-600'>SignIn</button>
-                                </div>
-                            </form>
+                            {
+                                (() => {
+                                    if(buttonValue === "SignIn"){
+                                        return (
+                                            <form onSubmit={headleSignIn}>
+                                                <div className="my-8">
+                                                    <label htmlFor="" className='my-2 lg:text-2xl'>Email : </label>
+                                                    <input type="email" name="" id="" className={`my-2 text-xl h-16 w-full rounded pl-2 mr-4  ${!Darkmode ? 'bg-gray-200 light-mode duration-500' : 'dark-mode duration-500 bg-[#1e293b]'}`} required placeholder='Enter Email Address'
+                                                    onChange={e => SetSignInData({...SignInData, email:e.target.value})}/>
+                                                </div>
+                                                <div className="my-8">
+                                                    <label htmlFor="" className='my-2 lg:text-2xl'>Password : </label>
+                                                    <input type="password" name="" id="" className={`my-2 text-xl h-16 w-full rounded pl-2 mr-4  ${!Darkmode ? 'bg-gray-200 light-mode duration-500' : 'dark-mode duration-500 bg-[#1e293b]'}`} required placeholder='Enter Password'
+                                                    onChange={e => SetSignInData({...SignInData, password:e.target.value})}/>
+                                                </div>
+                                                <div className="">
+                                                    <button type='submit' className='w-full h-14 bg-blue-500 rounded text-white duration-500 hover:bg-blue-600'>SignIn</button>
+                                                </div>
+                                            </form>
+                                        )
+                                    }
+
+                                    if(buttonValue === "SignUp"){
+                                        return (
+                                            <form onSubmit={headleSignIn}>
+                                                <div className="my-8">
+                                                    <label htmlFor="" className='my-2 lg:text-2xl'>Email : </label>
+                                                    <input type="email" name="" id="" className={`my-2 text-xl h-16 w-full rounded pl-2 mr-4  ${!Darkmode ? 'bg-gray-200 light-mode duration-500' : 'dark-mode duration-500 bg-[#1e293b]'}`} required placeholder='Enter Email Address'
+                                                    onChange={e => SetSignInData({...SignInData, email:e.target.value})}/>
+                                                </div>
+                                                <div className="my-8">
+                                                    <label htmlFor="" className='my-2 lg:text-2xl'>Password : </label>
+                                                    <input type="password" name="" id="" className={`my-2 text-xl h-16 w-full rounded pl-2 mr-4  ${!Darkmode ? 'bg-gray-200 light-mode duration-500' : 'dark-mode duration-500 bg-[#1e293b]'}`} required placeholder='Enter Password'
+                                                    onChange={e => SetSignInData({...SignInData, password:e.target.value})}/>
+                                                </div>
+                                                <div className="">
+                                                    <button type='submit' className='w-full h-14 bg-blue-500 rounded text-white duration-500 hover:bg-blue-600'>SignIn</button>
+                                                </div>
+                                            </form>
+                                        )
+                                    }
+                                })()
+                            }
+
 
                             <div className="my-1 mb-8">
-                                <span><Link><span className='text-blue-600'>Forget Password</span></Link></span>
+                                {
+                                    (() => {
+                                        if(buttonValue === "SignIn"){
+                                            return (
+                                                <span className='text-blue-600'>Forget Password</span>
+                                            )
+                                        }
+                                        else{
+                                            <div className=""></div>
+                                        }
+                                    })()
+                                }
+                                
                             </div>
                             <div className="">
-                                <p>Don't have an Account ? <span className='text-blue-600 cursor-pointer'>SignUp</span></p>
+                                {
+                                    (() => {
+                                        if(buttonValue === "SignIn"){
+                                            return (
+                                                <p>Don't have an Account ? <span onClick={() => HeadleButtonClick("SignUp")} className='text-blue-600 cursor-pointer'>SignUp</span></p>
+                                            )
+                                        }
+                                        if(buttonValue === "SignUp"){
+                                            return (
+                                                <p>Already have an Account ? <span onClick={() => HeadleButtonClick("SignIn")} className='text-blue-600 cursor-pointer'>SignIn</span></p>
+                                            )
+                                        }
+                                    })()
+                                }
+                                
                             </div>
                         </div>
                     </div>
