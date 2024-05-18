5bae30034c4d5c9178f4fd2c45f9827f0c350e23
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-18 18:25:29 +0530

    Update App.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignInSignUp from "./comportment/SignInSignUp/SignInSignUp";
 import Dashboard from "./comportment/Dashboard/Dashboard";
+import PrivateRoute from "./comportment/Security/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignInSignUp />} />
-        
-        <Route path="/Dashboard" element={<Dashboard />} />
+
+        <Route path="/Dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
       </Routes>
     </BrowserRouter>
   )
