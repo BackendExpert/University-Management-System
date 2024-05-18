1171ed7bfe6031d9aa3e83b2a8eca01466689000
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-18 20:34:59 +0530

    Update DashSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/DashSide.jsx
+++ client/src/comportment/Dashboard/DashSide.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import  secureLocalStorage  from  "react-secure-storage"
+import MyIcons from '@reacticons/ionicons'
 
 const DashSide = () => {
     const navigate = useNavigate()
@@ -8,30 +9,35 @@ const DashSide = () => {
     const RoleUser = secureLocalStorage.getItem("Login1");
     const EmailUser = secureLocalStorage.getItem("login2");
 
-    return (
-        <div className="w-64 h-screen bg-gray-800 text-white fixed">
-          <div className="p-4">
-            {
-                (() => {
-                    if(RoleUser === "SuperAdmin") {
-                        return (
-                            <h2 className="text-2xl font-bold">SuperAdmin Dashboard</h2>
-                        )
-                    }
-                    if(RoleUser === "user"){
-                        return (
-                            <h2 className="text-2xl font-bold">Dashboard</h2>
-                        ) 
-                    }
-                })()
-            }
-            
-          </div>
-          <nav className="mt-10">
+    const [sideOpen, SetsideOpen] = useState(true)
 
-          </nav>
+    return (
+        <div className="">
+            <div className={` ${sideOpen ? 'w-72' : 'w-20'} duration-500 bg-gray-800 h-screen px-4 text-white`}>
+                <div className="lg:invisible visible">
+                {
+                    sideOpen ? 
+                        <p className="" onClick={() => SetsideOpen(false)}><MyIcons name='arrow-back-circle' size='large'></MyIcons></p>
+                    :
+                        <p className="" onClick={() => SetsideOpen(true)}><MyIcons name='arrow-forward-circle' size='large'></MyIcons></p>    
+                }
+                </div>
+                
+                <div className="flex">
+                    <p className="text-2xl"><MyIcons name='school' size='large'></MyIcons></p>                    
+                    {
+                        sideOpen ? 
+                            <h1 className="text-2xl my-2 pl-2">ABC Campus</h1>
+                        :
+                            <h1 className="text-2xl my-2 pl-2"></h1>
+
+                    }
+                    
+                </div>
+                
+            </div>
         </div>
-      );
+    )
     
 }
 
