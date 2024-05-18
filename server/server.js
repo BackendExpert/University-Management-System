7be7861a854324fe7557355cbced2e21379b5374
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-18 17:17:00 +0530

    Create server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bcrypt = require('bcryptjs');
+const multer = require('multer');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+require('dotenv').config(); // Load environment variables
+const fs = require('fs');
+const { parse } = require('json2csv');
+
+const path = require('path')
+
+const resourceLimits = require('worker_threads');
+const e = require('express');
+const { stat } = require('fs');
+
+
+const app = express();
+const PORT = process.env.PORT || 8081
+
+// all endpoint start
+
+
+// all endpoints end
+
+app.listen(PORT, () => console.log(`Server is Running on PORT ${PORT}`));
