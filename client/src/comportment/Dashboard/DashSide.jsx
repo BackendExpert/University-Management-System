38b6042f8aad01e064ab77049b487da60a659909
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-18 20:50:03 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/DashSide.jsx
+++ client/src/comportment/Dashboard/DashSide.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import  secureLocalStorage  from  "react-secure-storage"
 import MyIcons from '@reacticons/ionicons'
+import SuperAdminImg from '../../assets/SuperAdminWhite.png'
 
 const DashSide = () => {
     const navigate = useNavigate()
@@ -10,6 +11,15 @@ const DashSide = () => {
     const EmailUser = secureLocalStorage.getItem("login2");
 
     const [sideOpen, SetsideOpen] = useState(true)
+
+    const SideLink = [
+        {name: "Dashboard", link: "#", icons: "person"},
+        {name: "Students", link: "#", icons: "person"},
+        {name: "Students", link: "#", icons: "person"},
+        {name: "Students", link: "#", icons: "person"},
+        {name: "Students", link: "#", icons: "person"},       
+        
+    ]
 
     return (
         <div className="">
@@ -24,15 +34,37 @@ const DashSide = () => {
                 </div>
                 
                 <div className="flex">
-                    <p className="text-2xl"><MyIcons name='school' size='large'></MyIcons></p>                    
+                    <p className="text-2xl my-4"><MyIcons name='school' size='large'></MyIcons></p>                    
                     {
                         sideOpen ? 
-                            <h1 className="text-2xl my-2 pl-2">ABC Campus</h1>
+                            <div className="my-4">
+                                <h1 className="text-2xl my-2 pl-2">ABC Campus</h1>
+                                <p>SuperAdmin</p>
+                            </div>
+
                         :
                             <h1 className="text-2xl my-2 pl-2"></h1>
 
                     }
                     
+                </div>
+                <hr />
+                
+                <div className="">
+                    {
+                        SideLink.map((AdminLinks) => {
+                            return (
+                                <Link>
+                                    <div className="my-8 mx-2">
+                                        <div className="flex">
+                                            <p className=""><MyIcons name={AdminLinks.icons} size='large'></MyIcons></p>
+                                            <p className={` ${sideOpen ? 'visible' : 'invisible'} py-2 pl-4 text-xl`}>{AdminLinks.name}</p>
+                                        </div>
+                                    </div>
+                                </Link>
+                            )
+                        })
+                    }
                 </div>
                 
             </div>
