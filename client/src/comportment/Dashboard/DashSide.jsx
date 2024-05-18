c4b4d44137fc1c9dab3f4502db6114c1b1ceadbc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-18 19:59:46 +0530

    Update DashSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/DashSide.jsx
+++ client/src/comportment/Dashboard/DashSide.jsx
@@ -1,22 +1,34 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import  secureLocalStorage  from  "react-secure-storage"
 
 const DashSide = () => {
+    const navigate = useNavigate()
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
     return (
         <div className="w-64 h-screen bg-gray-800 text-white fixed">
           <div className="p-4">
-            <h2 className="text-2xl font-bold">Admin Dashboard</h2>
+            {
+                (() => {
+                    if(RoleUser === "SuperAdmin") {
+                        return (
+                            <h2 className="text-2xl font-bold">SuperAdmin Dashboard</h2>
+                        )
+                    }
+                    if(RoleUser === "user"){
+                        return (
+                            <h2 className="text-2xl font-bold">Dashboard</h2>
+                        ) 
+                    }
+                })()
+            }
+            
           </div>
           <nav className="mt-10">
-            <Link to="/dashboard" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">
-                Home
-            </Link>
-            <Link to="home" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">
-              Home
-            </Link>
-            <Link to="profile" className="block py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">
-              Profile
-            </Link>
+
           </nav>
         </div>
       );
