5aae78c0a488f70d7d723165f331c4af323a16a3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-19 18:34:08 +0530

    Update DashNav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/DashNav.jsx
+++ client/src/comportment/Dashboard/DashNav.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+
 
 const DashNav = () => {
+    const navigate = useNavigate()
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+    const DarkMode = localStorage.getItem('darkMode');
+
+    if(RoleUser !== null && EmailUser !== null){
+
+    }
+    else{
+        
+    }
   return (
-    <div>DashNav</div>
+    <div>
+        <div className="flex">
+
+        </div>
+    </div>
   )
 }
 
