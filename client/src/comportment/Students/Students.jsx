7b269b5f5414bcbf595bf227b82da76aeb6f5ee8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-19 19:22:42 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Students/Students.jsx
+++ client/src/comportment/Students/Students.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Students = () => {
   return (
-    <div>Students</div>
+    <div className='mx-8'>
+        <div className="my-4">
+            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Students </p>
+        </div>
+    </div>
   )
 }
 
--- client/src/comportment/Teachers/Teachers.jsx
+++ client/src/comportment/Teachers/Teachers.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Teachers = () => {
   return (
-    <div>Teachers</div>
+    <div className='mx-8'>
+        <div className="my-4">
+            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Teachers </p>
+        </div>
+    </div>
   )
 }
 
