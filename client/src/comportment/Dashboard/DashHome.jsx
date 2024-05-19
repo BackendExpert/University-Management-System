9e073c8bed277e1a72e8ecdd4e5b16b857e20956
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-19 19:02:48 +0530

    Update DashHome.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/DashHome.jsx
+++ client/src/comportment/Dashboard/DashHome.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const DashHome = () => {
   return (
-    <div className=''>DashHome</div>
+    <div className=''>
+        <div className="lg:grid grid-cols-4 gap-4">
+            <div className="">
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
