eb5953dba530ebb0ecac0f22fd031facfea55cf8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-19 19:04:05 +0530

    Update DashHome.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/DashHome.jsx
+++ client/src/comportment/Dashboard/DashHome.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import  secureLocalStorage  from  "react-secure-storage"
 
 const DashHome = () => {
-  return (
-    <div className=''>
-        <div className="lg:grid grid-cols-4 gap-4">
-            <div className="">
-                
+    const navigate = useNavigate()
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+    // const DarkMode = localStorage.getItem('darkMode');
+
+
+    if(RoleUser !== null && EmailUser !== null){
+        return (
+            <div className=''>
+                <div className="lg:grid grid-cols-4 gap-4">
+                    <div className="">
+        
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-  )
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
+
 }
 
 export default DashHome
