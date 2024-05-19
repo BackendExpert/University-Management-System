164e797325aacd7fcd7d7c34ec0a00380eb06d26
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-19 19:24:29 +0530

    Update MyProfile.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/MyStats/MyProfile.jsx
+++ client/src/comportment/MyStats/MyProfile.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const MyProfile = () => {
   return (
-    <div>MyProfile</div>
+    <div className='mx-8'>
+        <div className="my-4">
+            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Students </p>
+        </div>
+    </div>
   )
 }
 
