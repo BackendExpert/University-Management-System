1ab3188940f7f3bb932f51edf20f0693217e862b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-19 18:31:35 +0530

    Update Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/Dashboard.jsx
+++ client/src/comportment/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import DashSide from './DashSide';
 import { Outlet } from 'react-router-dom';
+import DashNav from './DashNav';
 
 
 const Dashboard = () => {
@@ -8,6 +9,7 @@ const Dashboard = () => {
     <div className="flex">
       <DashSide />
       <div className="flex-1 p-6">
+        <DashNav />
         <Outlet />
       </div>
     </div>
