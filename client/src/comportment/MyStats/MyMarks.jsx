f56d810f33979dd80ce444817dc6835ad4a32470
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-20 13:59:07 +0530

    Update MyMarks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/MyStats/MyMarks.jsx
+++ client/src/comportment/MyStats/MyMarks.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const MyMarks = () => {
   return (
-    <div>MyMarks</div>
+    <div className='mx-8'>
+        <div className="my-4">
+            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / MyProfile </p>
+        </div>
+    </div>
   )
 }
 
