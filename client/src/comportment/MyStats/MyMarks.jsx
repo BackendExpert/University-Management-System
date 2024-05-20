b247fc0b67a22f46b7cf2240c7b298842fd15838
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-20 13:59:27 +0530

    Update MyMarks.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/MyStats/MyMarks.jsx
+++ client/src/comportment/MyStats/MyMarks.jsx
@@ -6,7 +6,7 @@ const MyMarks = () => {
   return (
     <div className='mx-8'>
         <div className="my-4">
-            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / MyProfile </p>
+            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / My Marks </p>
         </div>
     </div>
   )
