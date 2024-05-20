123f628975be63d6557bc1f8fb2462a8497b5699
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-20 15:13:16 +0530

    Update DashHome.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/DashHome.jsx
+++ client/src/comportment/Dashboard/DashHome.jsx
@@ -4,7 +4,7 @@ import  secureLocalStorage  from  "react-secure-storage"
 import CountUp from 'react-countup'
 import UserImg from '../../assets/SuperAdmin.png'
 
-import { BsBackpack2Fill, BsSpeedometer2, BsPersonVideo3, BsFileEarmarkText, BsPeople, BsPersonGear, BsPower } from "react-icons/bs";
+import { BsBackpack2Fill, BsSpeedometer2, BsPersonVideo3, BsFileEarmarkText, BsPeople, BsPersonGear, BsPower, BsMortarboard } from "react-icons/bs";
 
 const DashHome = () => {
     const navigate = useNavigate()
@@ -19,9 +19,9 @@ const DashHome = () => {
         {id: 2, name: "Teachers", icon: <BsPersonVideo3 />, value: <CountUp end={20} />, style: "text-blue-500"},
         {id: 3, name: "Subjects", icon: <BsFileEarmarkText />, value: <CountUp end={20} />, style: "text-yellow-500"},
         {id: 4, name: "Staff", icon: <BsPeople />, value: <CountUp end={20} />, style: "text-red-500"},
-        {id: 5, name: "Students", icon: <BsBackpack2Fill />, value: <CountUp end={20} />, style: "text-purple-500"},
-        {id: 6, name: "Students", icon: <BsBackpack2Fill />, value: <CountUp end={20} />, style: "text-yellow-500"},
-        {id: 7, name: "Students", icon: <BsBackpack2Fill />, value: <CountUp end={20} />, style: "text-green-500"},
+        {id: 5, name: "Courses", icon: <BsMortarboard />, value: <CountUp end={20} />, style: "text-purple-500"},
+        {id: 6, name: "Departments", icon: <BsBackpack2Fill />, value: <CountUp end={20} />, style: "text-yellow-500"},
+
     ]
 
     if(RoleUser !== null && EmailUser !== null){
@@ -31,6 +31,23 @@ const DashHome = () => {
                     <p>/ Dashbord /</p>
                 </div>
                 <div className={` ${RoleUser === "Student" ? 'lg:flex' : ''}`}>
+                {
+                        (() => {
+                            if(RoleUser === "Student"){
+                                return (
+                                    <div className="mr-8 my-2 w-full">
+                                        <div className="bg-white  py-4 px-6 rounded shadow-md">
+                                            <h1 className="text-2xl font-semibold">My Information</h1>
+                                            <div className="py-8 px-6">
+                                                <img src="https://cdn-icons-png.flaticon.com/128/2641/2641333.png" alt="" className='h-40'/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        })()
+                    }
+
                     <div className={`lg:grid ${RoleUser === "Student" ? ' grid-cols-2 w-full' : ' grid-cols-4' } gap-4 my-2`}>
                         {
                             DashData.map((dataDash) => {
@@ -55,22 +72,6 @@ const DashHome = () => {
                             })
                         }
                     </div>
-                    {
-                        (() => {
-                            if(RoleUser === "Student"){
-                                return (
-                                    <div className="mx-4 my-2 w-full">
-                                        <div className="bg-white  py-4 px-6 rounded shadow-md">
-                                            <h1 className="text-2xl font-semibold">My Information</h1>
-                                            <div className="py-8 px-6">
-                                                <img src="https://cdn-icons-png.flaticon.com/128/2641/2641333.png" alt="" className='h-40'/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            }
-                        })()
-                    }
 
                 </div>
 
