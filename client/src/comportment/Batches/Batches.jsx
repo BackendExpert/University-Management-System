bf521af47c287754ab0e5bd4de7475500033b6e1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-20 19:25:39 +0530

    Create Batches.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/comportment/Batches/Batches.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Batches = () => {
+  return (
+    <div className="mx-8">
+        <div className="my-4">
+            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Batches </p>
+        </div>
+    </div>
+  )
+}
+
+export default Batches
