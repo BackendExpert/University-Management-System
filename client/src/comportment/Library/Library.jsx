90769726b3e5db903e48eea740baed099638f17a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-20 13:46:15 +0530

    Update Library.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Library/Library.jsx
+++ client/src/comportment/Library/Library.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Library = () => {
   return (
-    <div>Library</div>
+    <div className="mx-8">
+        <div className="my-4">
+            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Library </p>
+        </div>
+    </div>
   )
 }
 
