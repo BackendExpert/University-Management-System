8e197d76c4bb60d7f7738fb72a05a21032ece913
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-20 19:22:00 +0530

    Create Courses.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/comportment/Courses/Courses.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Courses = () => {
+  return (
+    <div className="mx-8">
+        <div className="my-4">
+            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Courses </p>
+        </div>
+    </div>
+  )
+}
+
+export default Courses
