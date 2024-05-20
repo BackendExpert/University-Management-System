019cf71cb026d8f19f021c5a67bf33aebf14076b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-20 19:21:30 +0530

    Create Departments.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/comportment/Departments/Departments.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Departments = () => {
+  return (
+    <div className="mx-8">
+        <div className="my-4">
+            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Departments </p>
+        </div>
+    </div>
+  )
+}
+
+export default Departments
