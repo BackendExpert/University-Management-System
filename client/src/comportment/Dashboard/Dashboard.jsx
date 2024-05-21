6388774aaf4073070e5d849e22e44995ed97ea63
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-21 17:48:12 +0530

    Update Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/Dashboard.jsx
+++ client/src/comportment/Dashboard/Dashboard.jsx
@@ -2,17 +2,26 @@ import React from 'react'
 import DashSide from './DashSide';
 import { Outlet } from 'react-router-dom';
 import DashNav from './DashNav';
+import DashSideTest from './DashSideTest';
 
 
 const Dashboard = () => {
   return (
     <div className="flex">
-      <DashSide />
-      <div className="flex-1 px-2 py-4 bg-gray-200">
-        <DashNav />
-        <Outlet />
-      </div>
+    <DashSideTest />
+    <div className="flex-1 p-8">
+      <h1 className="text-3xl font-bold">Main Content</h1>
+      <p>Welcome to the main content area.</p>
     </div>
+  </div>
+    // <div className="">
+    //   {/* <DashSide /> */}
+    //   <DashSideTest />
+    //   <div className="flex-1 px-2 py-4 bg-gray-200">
+    //     <DashNav />
+    //     <Outlet />
+    //   </div>
+    // </div>
   )
 }
 
