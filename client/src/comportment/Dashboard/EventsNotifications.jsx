d57f1de0b54ccef7a8c95c025b078b36b5f2bdb7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-22 11:21:39 +0530

    Update EventsNotifications.jsx
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/EventsNotifications.jsx
+++ client/src/comportment/Dashboard/EventsNotifications.jsx
@@ -11,8 +11,108 @@ const EventsNotifications = () => {
 
     if(RoleUser !== null && EmailUser !== null) {
         return (
-            <div>
-        
+            <div className='my-4'>
+                <div className="md:grid grid-cols-2 gap-4">
+                    <div className="bg-white py-4 px-6 rounded shadow-md">
+                        <h1 className="text-gray-500 font-semibold text-xl">Notifications</h1>
+                        <hr className='my-4'/>           
+                    
+
+                        <div class="relative overflow-x-auto">
+                            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <tbody>
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Subject
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            Subject ID : IT/45/14 Successfully Added
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            2024/5/22 10:56
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            $2999
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+
+                    </div>
+                    <div className="bg-white py-4 px-6 rounded shadow-md">
+                    <h1 className="text-gray-500 font-semibold text-xl">Events</h1>
+                        <hr className='my-4'/>           
+                    
+
+                        <div class="relative overflow-x-auto">
+                            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" class="px-6 py-3">
+                                            Product name
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Color
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Category
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Price
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Apple MacBook Pro 17"
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            Silver
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            Laptop
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            $2999
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Microsoft Surface Pro
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            White
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            Laptop PC
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            $1999
+                                        </td>
+                                    </tr>
+                                    <tr class="bg-white dark:bg-gray-800">
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            Magic Mouse 2
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            Black
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            Accessories
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            $99
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         )
     }
