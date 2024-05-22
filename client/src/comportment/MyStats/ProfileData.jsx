1ef86777e9a0d3e561f95019c6c6a2b06909fa4e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-22 14:59:54 +0530

    Create ProfileData.jsx
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ client/src/comportment/MyStats/ProfileData.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup'
+import UserImg from '../../assets/SuperAdmin.png'
+
+const ProfileData = () => {
+    const navigate = useNavigate()
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    
+  return (
+    <div>ProfileData</div>
+  )
+}
+
+export default ProfileData
