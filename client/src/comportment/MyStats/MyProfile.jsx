7f9b0bbc9763dce1aec37d5163259c56ea115fbd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-22 14:41:08 +0530

    Update MyProfile.jsx
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/MyStats/MyProfile.jsx
+++ client/src/comportment/MyStats/MyProfile.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup'
+import UserImg from '../../assets/SuperAdmin.png'
 
 const MyProfile = () => {
+    const navigate = useNavigate()
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
   return (
-    <div className='mx-8'>
+    <div className='mx-2'>
         <div className="my-4">
             <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / MyProfile </p>
+        </div>
+
+
+
+        <div className="">
+          <div className="lg:flex ">
+            <div className="w-full">
+              <div className="bg-white py-4 px-8 rounded shadow-md">
+                ashdkjh 
+              </div>
+              <div className="bg-white py-4 px-8 my-4 rounded shadow-md">
+                ashdkjh
+              </div>
+            </div>
+            <div className="md:mx-4 mx-0 w-full md:my-0 my-4">
+              <div className="bg-white py-4 px-8  rounded shadow-md">
+                ashdkjh
+              </div>
+            </div>
+          </div>
         </div>
     </div>
   )
