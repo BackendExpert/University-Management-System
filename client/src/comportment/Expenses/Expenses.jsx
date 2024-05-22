bea50236f8a52650176b36f169343febe8b26233
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-22 14:18:02 +0530

    Update Expenses.jsx
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Expenses/Expenses.jsx
+++ client/src/comportment/Expenses/Expenses.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Expenses = () => {
   return (
-    <div>Expenses</div>
+    <div className='mx-8'>
+        <div className="my-4">
+            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / My Payments </p>
+        </div>
+    </div>
   )
 }
 
