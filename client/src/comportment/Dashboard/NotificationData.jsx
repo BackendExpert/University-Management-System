cd4560a5f6e831a6218c2eac75f9e380aba53a9d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-22 12:37:56 +0530

    Update NotificationData.jsx
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/NotificationData.jsx
+++ client/src/comportment/Dashboard/NotificationData.jsx
@@ -7,9 +7,6 @@ const NotificationData = () => {
         {title: "Notification 1", NBody: "Body of Notification", NDate: "2024/05/22 10:30"},
         {title: "Notification 1", NBody: "Body of Notification", NDate: "2024/05/22 10:30"},
         {title: "Notification 1", NBody: "Body of Notification", NDate: "2024/05/22 10:30"},
-        {title: "Notification 1", NBody: "Body of Notification", NDate: "2024/05/22 10:30"},
-        {title: "Notification 1", NBody: "Body of Notification", NDate: "2024/05/22 10:30"},
-        {title: "Notification 1", NBody: "Body of Notification", NDate: "2024/05/22 10:30"},
         
     ]
   return (
@@ -17,10 +14,14 @@ const NotificationData = () => {
         {
             notifi.map((no, index) => {
                 return (
-                    <div className="my-2 mx-4 bg-white">
-                        <div className="flex">
-                            <h1 className="">{no.title}</h1>
+                    <div className="my-2 mx-4 bg-white py-4 px-8 rounded shadow-md">
+                        <div className="flex justify-between">
+                            <h1 className="text-gray-500">{no.title}</h1>
                             <p className="">{no.NDate}</p>
+                        </div>
+                        <hr />
+                        <div className="py-2 px-8">
+                            {no.NBody}
                         </div>
                     </div>
                 )
