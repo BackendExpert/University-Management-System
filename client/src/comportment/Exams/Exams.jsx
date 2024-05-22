9c9c837753ce29e17dfbb4b632635ff396bbfddb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-22 13:28:03 +0530

    Update Exams.jsx
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Exams/Exams.jsx
+++ client/src/comportment/Exams/Exams.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Exams = () => {
   return (
-    <div>Exams</div>
+    <div className='mx-8'>
+        <div className="my-4">
+            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Staff </p>
+        </div>
+    </div>
   )
 }
 
