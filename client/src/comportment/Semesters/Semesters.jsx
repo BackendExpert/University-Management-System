f72b4409e7d2c4a9508c62646d978cfe4a92e5a5
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-22 13:27:47 +0530

    Update Semesters.jsx
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Semesters/Semesters.jsx
+++ client/src/comportment/Semesters/Semesters.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Semesters = () => {
   return (
-    <div>Semesters</div>
+    <div className='mx-8'>
+        <div className="my-4">
+            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Staff </p>
+        </div>
+    </div>
   )
 }
 
