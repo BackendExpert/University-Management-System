401e2c5e9c997baed59dff458d47a5f7a8790aee
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-22 11:09:17 +0530

    Update EventsNotifications.jsx
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/EventsNotifications.jsx
+++ client/src/comportment/Dashboard/EventsNotifications.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import  secureLocalStorage  from  "react-secure-storage"
+import MyIcons from '@reacticons/ionicons'
 
 const EventsNotifications = () => {
-  return (
-    <div>
+    const navigate = useNavigate()
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    if(RoleUser !== null && EmailUser !== null) {
+        return (
+            <div>
         
-    </div>
-  )
+            </div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default EventsNotifications
