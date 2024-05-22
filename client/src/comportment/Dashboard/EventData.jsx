c844b321fe4c968026a3022985809ad8dd5e32b2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-22 12:39:19 +0530

    Update EventData.jsx
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/EventData.jsx
+++ client/src/comportment/Dashboard/EventData.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
 const EventData = () => {
+    const notifi = [
+        {title: "Notification 1", NBody: "Body of Notification", NDate: "2024/05/22 10:30"},
+        {title: "Notification 1", NBody: "Body of Notification", NDate: "2024/05/22 10:30"},
+        {title: "Notification 1", NBody: "Body of Notification", NDate: "2024/05/22 10:30"},
+        {title: "Notification 1", NBody: "Body of Notification", NDate: "2024/05/22 10:30"},
+        {title: "Notification 1", NBody: "Body of Notification", NDate: "2024/05/22 10:30"},
+        
+    ]
   return (
-    <div>EventData</div>
+    <div>
+        {
+            notifi.map((no, index) => {
+                return (
+                    <div className="my-2 mx-4 bg-white py-4 px-8 rounded shadow-md">
+                        <div className="flex justify-between">
+                            <h1 className="text-gray-500">{no.title}</h1>
+                            <p className="">{no.NDate}</p>
+                        </div>
+                        <hr />
+                        <div className="py-2 px-8">
+                            {no.NBody}
+                        </div>
+                    </div>
+                )
+            })
+        }
+    </div>
   )
 }
 
