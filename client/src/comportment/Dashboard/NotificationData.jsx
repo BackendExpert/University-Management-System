7329cd2199ef37bc4554bbfc904f74914162756a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-22 12:28:34 +0530

    Update NotificationData.jsx
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/NotificationData.jsx
+++ client/src/comportment/Dashboard/NotificationData.jsx
@@ -2,7 +2,48 @@ import React from 'react'
 
 const NotificationData = () => {
   return (
-    <div></div>
+    <div>
+        <div className="flex flex-col">
+            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                <div className="overflow-hidden">
+                    <table
+                    className="min-w-full text-left text-sm font-light text-surface dark:text-white">
+                    <thead
+                        className="border-b border-neutral-200 font-medium dark:border-white/10">
+                        <tr>
+                        <th scope="col" className="px-6 py-4">#</th>
+                        <th scope="col" className="px-6 py-4">First</th>
+                        <th scope="col" className="px-6 py-4">Last</th>
+                        <th scope="col" className="px-6 py-4">Handle</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="border-b border-neutral-200 dark:border-white/10">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
+                        <td className="whitespace-nowrap px-6 py-4">Mark</td>
+                        <td className="whitespace-nowrap px-6 py-4">Otto</td>
+                        <td className="whitespace-nowrap px-6 py-4">@mdo</td>
+                        </tr>
+                        <tr className="border-b border-neutral-200 dark:border-white/10">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">2</td>
+                        <td className="whitespace-nowrap px-6 py-4">Jacob</td>
+                        <td className="whitespace-nowrap px-6 py-4">Thornton</td>
+                        <td className="whitespace-nowrap px-6 py-4">@fat</td>
+                        </tr>
+                        <tr className="border-b border-neutral-200 dark:border-white/10">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">3</td>
+                        <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                        <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                        <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                        </tr>
+                    </tbody>
+                    </table>
+                </div>
+                </div>
+            </div>
+            </div>
+    </div>
   )
 }
 
