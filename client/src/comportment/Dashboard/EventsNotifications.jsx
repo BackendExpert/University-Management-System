c45041b3c8194de22fd5ba7edf15c46156f8282e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-22 12:16:10 +0530

    Update EventsNotifications.jsx
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/EventsNotifications.jsx
+++ client/src/comportment/Dashboard/EventsNotifications.jsx
@@ -14,28 +14,43 @@ const EventsNotifications = () => {
         SetButtonValue(clickValue)   
     }
 
+    // test notifications
+    const Notifications = [
+        {title: 'Subject', bodyN: 'The Subject 4894 has been Added', data: '2024/05/22 10:30'},
+        {title: 'Subject', bodyN: 'The Subject 894  has been Added', data: '2024/05/22 10:30'},
+        {title: 'Subject', bodyN: 'The Subject 4894 has been Added', data: '2024/05/22 10:30'},
+        {title: 'Subject', bodyN: 'The Subject 4894 has been Added', data: '2024/05/22 10:30'},
+        {title: 'Subject', bodyN: 'The Subject 4894 has been Added', data: '2024/05/22 10:30'},
+        {title: 'Subject', bodyN: 'The Subject 4894 has been Added', data: '2024/05/22 10:30'},
+        {title: 'Subject', bodyN: 'The Subject 4894 has been Added', data: '2024/05/22 10:30'},        
+    ]
+
     if(RoleUser !== null && EmailUser !== null) {
         return (
             <div>
                 <div className="bg-white py-4 px-6 my-4">
-                    <div className="flex">
                         {
                             (() => {
                                 if(buttonValue === 0){
                                     return (
-                                        <div className="mx-2">Notifications</div>
+                                        <div className="flex">
+                                            <div className="cursor-pointer mx-2 py-2 border-b-2 border-blue-500">Notifications</div>
+                                            <div className="cursor-pointer mx-2 py-2" onClick={() => HeadleButtonClick("Events")}>Events</div> 
+                                        </div>
                                     )
                                 }
                                 if(buttonValue === "Events"){
                                     return (
-                                        <div className="mx-2">Events</div>    
+                                        <div className="flex">
+                                            <div className="cursor-pointer mx-2 py-2 " onClick={() => HeadleButtonClick(0)}>Notifications</div>
+                                            <div className="cursor-pointer mx-2 py-2 border-b-2 border-blue-500">Events</div> 
+                                        </div>
                                     )
                                 }
                             })()
                         }
-                        <div className="mx-2">Notifications</div>
-                        <div className="mx-2">Events</div>                        
-                    </div>
+
+                    <hr  className='my-4'/> 
                 </div>
             </div>
         )
