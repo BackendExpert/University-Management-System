9557378b1a25aa07cbaa8dccc32c1cdfef202834
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-22 11:01:44 +0530

    Update Notifications.jsx
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/MyStats/Notifications.jsx
+++ client/src/comportment/MyStats/Notifications.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Notifications = () => {
   return (
-    <div>Notifications</div>
+    <div className='mx-8'>
+        <div className="my-4">
+            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Notifications </p>
+        </div>
+    </div>
   )
 }
 
