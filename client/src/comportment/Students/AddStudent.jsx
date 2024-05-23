baa2c0b4e720ddd01a767fc0dcc17382229eee68
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-23 11:48:12 +0530

    Update AddStudent.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Students/AddStudent.jsx
+++ client/src/comportment/Students/AddStudent.jsx
@@ -20,13 +20,28 @@ const AddStudent = () => {
         AdmissionNo: '',
         gender: '',
     })
-    
+
 
     if(RoleUser !== null && EmailUser !== null && RoleUser !== "Student"){
         return (
             <div className='bg-white shadow-md rounded py-12 px-10 my-8'>
                 <h1 className="lg:text-3xl text-xl text-gray-500">Add New Student</h1>
                 <hr className='pb-4 pt-2'/>
+
+                <form>
+                    <div className="lg:grid grid-cols-4 gap-4">
+                        <div className="my-2">
+                            <label htmlFor="">Username: </label>
+                            <input type="text" name="" id="" className="h-12 rounded pl-2 border-none bg-gray-200" required placeholder='Enter Username'
+                            onChange={e => SetStdData({...StdData, username:e.target.value})}/>
+                        </div>
+                        <div className="">
+                            <label htmlFor="">Email: </label>
+                            <input type="email" name="" id="" className="h-12 rounded pl-2 border-none bg-gray-200" required placeholder='Enter Email Address'
+                            onChange={e}/>
+                        </div>
+                    </div>
+                </form>
             </div>
         )
     }
