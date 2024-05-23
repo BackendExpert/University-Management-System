f5484df31835fc80a974b65aa8192f8bdf01d48f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-23 11:21:15 +0530

    Update Students.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Students/Students.jsx
+++ client/src/comportment/Students/Students.jsx
@@ -27,7 +27,81 @@ const Students = () => {
     ]
 
     if(RoleUser !== null && EmailUser !== null && RoleUser !== "Student"){
-
+      return (
+        <div className='mx-4'>
+            <div className="my-4">
+                <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Students </p>
+            </div>
+    
+            <div className="">
+              <div className="md:grid grid-cols-4 gap-4">
+                {
+                  studentData.map((dataStd) => {
+                    if(RoleUser === "SuperAdmin") {
+                      if(dataStd.id !== 4 && dataStd.id !== 5){
+                        return (
+                          <div onClick={() => HeadleButtonClick(dataStd.btnValue)} className={`cursor-pointer bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${dataStd.style}`}>                                       
+                              <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
+                                  <div className="">
+                                      <h1 className={`text-[180%] ${dataStd.style}`}>{dataStd.value}</h1>
+                                      <p className="py-2 text-[120%]">{dataStd.name}</p>
+                                  </div>
+                                  <div className="">
+                                      <p className="text-[200%] text-gray-500">{dataStd.icon}</p>
+                                  </div>
+                              </div>
+                              <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
+                                  All {dataStd.name} 
+                              </div>
+                          </div> 
+                          )
+                      }
+                    }
+                    if(RoleUser === "Hod"){
+                      return (
+                        <div onClick={() => HeadleButtonClick(dataStd.btnValue)} className={`bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${dataStd.style}`}>                                       
+                            <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
+                                <div className="">
+                                    <h1 className={`text-[180%] ${dataStd.style}`}>{dataStd.value}</h1>
+                                    <p className="py-2 text-[120%]">{dataStd.name}</p>
+                                </div>
+                                <div className="">
+                                    <p className="text-[200%] text-gray-500">{dataStd.icon}</p>
+                                </div>
+                            </div>
+                            <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
+                                All {dataStd.name} 
+                            </div>
+                        </div> 
+                        )
+                    }
+                    if(RoleUser === "Lec"){
+                      if(dataStd.id === 1 || dataStd.id === 4 || dataStd.id === 5){
+                        return (
+                          <div className={`bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${dataStd.style}`}>                                       
+                              <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
+                                  <div className="">
+                                      <h1 className={`text-[180%] ${dataStd.style}`}>{dataStd.value}</h1>
+                                      <p className="py-2 text-[120%]">{dataStd.name}</p>
+                                  </div>
+                                  <div className="">
+                                      <p className="text-[200%] text-gray-500">{dataStd.icon}</p>
+                                  </div>
+                              </div>
+                              <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
+                                  All {dataStd.name} 
+                              </div>
+                          </div> 
+                          )
+                      }
+                    }
+    
+                  })
+                }
+              </div>
+            </div>
+        </div>
+      )
     }
     else{
       useEffect(() => {
@@ -35,81 +109,6 @@ const Students = () => {
         navigate('/')
       }, [])
     }
-  return (
-    <div className='mx-4'>
-        <div className="my-4">
-            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Students </p>
-        </div>
-
-        <div className="">
-          <div className="md:grid grid-cols-4 gap-4">
-            {
-              studentData.map((dataStd) => {
-                if(RoleUser === "SuperAdmin") {
-                  if(dataStd.id !== 4 && dataStd.id !== 5){
-                    return (
-                      <div onClick={() => HeadleButtonClick(dataStd.btnValue)} className={`cursor-pointer bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${dataStd.style}`}>                                       
-                          <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
-                              <div className="">
-                                  <h1 className={`text-[180%] ${dataStd.style}`}>{dataStd.value}</h1>
-                                  <p className="py-2 text-[120%]">{dataStd.name}</p>
-                              </div>
-                              <div className="">
-                                  <p className="text-[200%] text-gray-500">{dataStd.icon}</p>
-                              </div>
-                          </div>
-                          <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
-                              All {dataStd.name} 
-                          </div>
-                      </div> 
-                      )
-                  }
-                }
-                if(RoleUser === "Hod"){
-                  return (
-                    <div onClick={() => HeadleButtonClick(dataStd.btnValue)} className={`bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${dataStd.style}`}>                                       
-                        <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
-                            <div className="">
-                                <h1 className={`text-[180%] ${dataStd.style}`}>{dataStd.value}</h1>
-                                <p className="py-2 text-[120%]">{dataStd.name}</p>
-                            </div>
-                            <div className="">
-                                <p className="text-[200%] text-gray-500">{dataStd.icon}</p>
-                            </div>
-                        </div>
-                        <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
-                            All {dataStd.name} 
-                        </div>
-                    </div> 
-                    )
-                }
-                if(RoleUser === "Lec"){
-                  if(dataStd.id === 1 || dataStd.id === 4 || dataStd.id === 5){
-                    return (
-                      <div className={`bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${dataStd.style}`}>                                       
-                          <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
-                              <div className="">
-                                  <h1 className={`text-[180%] ${dataStd.style}`}>{dataStd.value}</h1>
-                                  <p className="py-2 text-[120%]">{dataStd.name}</p>
-                              </div>
-                              <div className="">
-                                  <p className="text-[200%] text-gray-500">{dataStd.icon}</p>
-                              </div>
-                          </div>
-                          <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
-                              All {dataStd.name} 
-                          </div>
-                      </div> 
-                      )
-                  }
-                }
-
-              })
-            }
-          </div>
-        </div>
-    </div>
-  )
 }
 
 export default Students
