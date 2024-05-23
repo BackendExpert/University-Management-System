bfbed83721e94ad12b752de7b44c9cf34950ff24
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-23 11:11:01 +0530

    Update DashSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Dashboard/DashSide.jsx
+++ client/src/comportment/Dashboard/DashSide.jsx
@@ -77,7 +77,7 @@ const DashSide = () => {
                     !isOpen ? <BsList /> : <BsX />
                 }
             </button>
-            <div className={`md:relative fixed top-0 left-0 h-full px-4 bg-gray-800 text-white w-auto transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
+            <div className={`lg:pr-16 md:relative fixed top-0 left-0 h-full px-4 bg-gray-800 text-white w-auto transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
                 <div className="flex whitespace-nowrap lg:py-8">
                     <p className="text-2xl my-4 "><MyIcons name='school' size='large'></MyIcons></p>                    
 
