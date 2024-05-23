3ff0c7d1402fdc463fc38bb7c0d4ec4935b652e6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-23 11:22:11 +0530

    Update AddStudent.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Students/AddStudent.jsx
+++ client/src/comportment/Students/AddStudent.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BsBag, BsPeople, BsPersonAdd, BsPersonGear, BsPersonPlusFill, BsPersonSlash } from 'react-icons/bs'
+import { Link, useNavigate } from 'react-router-dom'
+import CountUp from 'react-countup'
+import  secureLocalStorage  from  "react-secure-storage"
 
 const AddStudent = () => {
+    const navigate = useNavigate()
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+    const DarkMode = localStorage.getItem('darkMode');
   return (
     <div>AddStudent</div>
   )
