b3cf147a5e8ec0d6106e49c2d2c6bf2675ff6078
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-24 14:31:23 +0530

    Update ProfileData.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/MyStats/ProfileData.jsx
+++ client/src/comportment/MyStats/ProfileData.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import  secureLocalStorage  from  "react-secure-storage"
 import CountUp from 'react-countup'
 import UserImg from '../../assets/SuperAdmin.png'
+import axios from 'axios';
+
 
 const ProfileData = () => {
     const navigate = useNavigate()
@@ -27,25 +29,31 @@ const ProfileData = () => {
                 <div className="bg-white py-4 px-8 rounded shadow-md">
                 <div className="lg:py-16 py-8 md:flex">
                     <img src="https://cdn-icons-png.flaticon.com/128/2641/2641333.png" alt="" className='lg:h-56 h-[20%] w-auto'/>
-                      <table border={0} className='mx-2 lg:text-2xl'>
-                          <tr className=''>
-                              <td className='font-semibold'>Email: </td>
-                              <td className='pl-4 text-gray-500'>jehan.w@example.com </td>
-                          </tr>
-                          <tr>
-                              <td className='font-semibold'>Admission ID: </td>
-                              <td className='pl-4 text-gray-500'>123456ABCD </td>
-                          </tr>
-                          <tr>
-                              <td className='font-semibold'>Username: </td>
-                              <td className='pl-4 text-gray-500'>jehanstd </td>
-                          </tr>
-        
-                          <tr>
-                              <td className='font-semibold'>Password: </td>
-                              <td className='pl-4 text-gray-500'>**********</td>
-                          </tr>
-                      </table>
+                    {
+                      MyData.map((stdData, index) => {
+                        return (
+                          <table border={0} className='mx-2 lg:text-2xl'>
+                            <tr className=''>
+                                <td className='font-semibold'>Email: </td>
+                                <td className='pl-4 text-gray-500'>{EmailUser}</td>
+                            </tr>
+                            <tr>
+                                <td className='font-semibold'>Admission ID: </td>
+                                <td className='pl-4 text-gray-500'>{stdData.RegNo}</td>
+                            </tr>
+                            <tr>
+                                <td className='font-semibold'>Register At: </td>
+                                <td className='pl-4 text-gray-500'>{stdData.RegisterAt} </td>
+                            </tr>
+          
+                            <tr>
+                                <td className='font-semibold'>Password: </td>
+                                <td className='pl-4 text-gray-500'>**********</td>
+                            </tr>
+                        </table>
+                        )
+                      })
+                    }
                   </div>
                 </div>
                 <div className="bg-white py-4 px-8 my-4 rounded shadow-md">
