cc7cc3e4c1ee84583070e7f8a4b7b092a3ef90c8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 17:01:22 +0530

    Update Departments.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Departments/Departments.jsx
+++ client/src/comportment/Departments/Departments.jsx
@@ -12,7 +12,7 @@ const Departments = () => {
   const EmailUser = secureLocalStorage.getItem("login2");
 
   const DeptData = [
-    {name: "Departments", value: <CountUp end={20} />, icos: <BsBuilding />, style: "text-blue-500"}
+    {name: "Departments", btnValue: "AllDepts", value: <CountUp end={20} />, icos: <BsBuilding />, style: "text-blue-500"}
   ]
   if(EmailUser !== null && RoleUser === "SuperAdmin"){
     return (
@@ -20,6 +20,30 @@ const Departments = () => {
           <div className="my-4">
               <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Departments </p>
           </div>
+          <div className="">
+            <div className="md:grid grid-cols-3 gap-4 my-4 mr-4 ">
+              {
+                DeptData.map((Depts) => {
+                  return (
+                    <div className={`h-full bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${stdData.style}`}>                                       
+                      <div className="lg:py-8 flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
+                          <div className="">
+                              <h1 className={`text-[100%] ${stdData.style}`}>{stdData.value}</h1>
+                              <p className="py-2 text-[120%] lg:text-3xl lg:mt-2">{stdData.name}</p>
+                          </div>
+                          <div className="">
+                              <p className="lg:text-5xl text-[200%] text-gray-500">{stdData.icons}</p>
+                          </div>
+                      </div>
+                      <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
+                          {stdData.name} 
+                      </div>
+                  </div>  
+                  )
+                })
+              }
+            </div>
+          </div>  
       </div>
     )
   }
