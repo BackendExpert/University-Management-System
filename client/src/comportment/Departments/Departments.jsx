cb11fb239135c5b2cf27eaf4dd77b6d6cf1c215a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 16:55:26 +0530

    Update Departments.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Departments/Departments.jsx
+++ client/src/comportment/Departments/Departments.jsx
@@ -4,15 +4,27 @@ import  secureLocalStorage  from  "react-secure-storage"
 import CountUp from 'react-countup'
 import axios from 'axios';
 
-
 const Departments = () => {
-  return (
-    <div className="mx-8">
-        <div className="my-4">
-            <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Departments </p>
-        </div>
-    </div>
-  )
+  const navigate = useNavigate()
+  //curent login user
+  const RoleUser = secureLocalStorage.getItem("Login1");
+  const EmailUser = secureLocalStorage.getItem("login2");
+
+  if(EmailUser !== null && RoleUser === "SuperAdmin"){
+    return (
+      <div className="mx-8">
+          <div className="my-4">
+              <p>/ <Link to={'/Dashboard/Home'}> <span className='text-blue-500'>Dashbord</span></Link> / Departments </p>
+          </div>
+      </div>
+    )
+  }
+  else{
+    useEffect(() => {
+      localStorage.clear()
+      navigate('/')
+    }, [])
+  }
 }
 
 export default Departments
