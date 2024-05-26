a287f4a90e1c0b6b0c71e5b4a1dfa475e53f8f50
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 18:32:39 +0530

    Update AddDept.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Departments/AddDept.jsx
+++ client/src/comportment/Departments/AddDept.jsx
@@ -12,9 +12,34 @@ const AddDept = () => {
     const RoleUser = secureLocalStorage.getItem("Login1");
     const EmailUser = secureLocalStorage.getItem("login2");
 
+    // dept data
+    const [deptData, SetdeptData] = useState({
+        deptNo: '',
+        deptName: '',
+        deptDesc: ''
+    })
+
     if(EmailUser !== null && RoleUser === "SuperAdmin"){
         return (
-            <div>AddDept</div>
+            <div className='bg-white py-4 px-8 rounded shadow-md'>
+                <div className="md:grid grid-cols-2 gap-4">
+                    <div className="my-2">
+                        <label htmlFor="">Department ID</label>
+                        <input type="text" name="" id="" className="h-12 rounded pl-2 border-none bg-gray-200 w-full my-2" required placeholder='Enter Department ID' 
+                        onChange={e => SetdeptData({...deptData, deptNo:e.target.value})}/>
+                    </div>
+                    <div className="my-2">
+                        <label htmlFor="">Department Name</label>
+                        <input type="text" name="" id="" className="h-12 rounded pl-2 border-none bg-gray-200 w-full my-2" required placeholder='Enter Department Name' 
+                        onChange={e => SetdeptData({...deptData, deptName:e.target.value})}/>
+                    </div>
+                    <div className="my-2">
+                        <label htmlFor="">Department Description</label>
+                        <input type="text" name="" id="" className="h-12 rounded pl-2 border-none bg-gray-200 w-full my-2" required placeholder='Enter Department Description' 
+                        onChange={e => SetdeptData({...deptData, deptDesc:e.target.value})}/>
+                    </div>
+                </div>
+            </div>
         )
     }
     else{
