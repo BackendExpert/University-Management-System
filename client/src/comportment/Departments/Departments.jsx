52300b11e958c184cba8d9b19aea34dd3a15d256
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-26 16:52:22 +0530

    Update Departments.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Departments/Departments.jsx
+++ client/src/comportment/Departments/Departments.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup'
+import axios from 'axios';
 
 
 const Departments = () => {
