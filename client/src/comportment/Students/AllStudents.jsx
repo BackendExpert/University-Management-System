3ae22470366b9d6470802c31117df10f6d2e1265
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-28 10:35:26 +0530

    Update AllStudents.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Students/AllStudents.jsx
+++ client/src/comportment/Students/AllStudents.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import  secureLocalStorage  from  "react-secure-storage"
+import axios from 'axios';
+
 
 const AllStudents = () => {
+    const navigate = useNavigate()
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+
+    if(RoleUser !== null && EmailUser !== null & RoleUser === "SuperAdmin" && RoleUser === "Hod")
   return (
     <div>AllStudents</div>
   )
