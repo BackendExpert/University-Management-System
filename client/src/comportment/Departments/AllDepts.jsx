205a4ef6d3ebcdd41a330cc8313d21f95a1128f6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-28 09:51:25 +0530

    Update AllDepts.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Departments/AllDepts.jsx
+++ client/src/comportment/Departments/AllDepts.jsx
@@ -1,40 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup'
+import axios from 'axios';
+import { BsBuilding, BsBuildingAdd } from 'react-icons/bs';
+
+
+// DeptsView
+
 
 const AllDepts = () => {
-  return (
-    <div className="">
-        <div className='bg-white py-4 px-8 rounded shadow-md'>
-            <h1 className="lg:text-3xl text-xl text-gray-500">Departments</h1>
-            <hr className='pb-4 pt-2'/>
-        </div>
+    const navigate = useNavigate()
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
 
-        <div class="overflow-x-auto my-8">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" class="px-6 py-3 md:block hidden">
-                            Dept No
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Dept Name 
-                        </th>
-                        <th scope="col" class="px-6 py-3 md:block hidden">
-                            Dept Descriptiono
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Action
-                        </th>
-                    </tr>            
-                </thead>
-                <tbody>
-
-                </tbody>
-            </table>
-        </div>   
-
-    </div>
-
-    )
+    if(RoleUser !== null && EmailUser !== null && RoleUser === "SuperAdmin"){
+        return (
+            <div className="">
+                <div className='bg-white py-4 px-8 rounded shadow-md'>
+                    <h1 className="lg:text-3xl text-xl text-gray-500">Departments</h1>
+                    <hr className='pb-4 pt-2'/>
+                </div>
+        
+                <div class="overflow-x-auto my-8">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3 md:block hidden">
+                                    Dept No
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Dept Name 
+                                </th>
+                                <th scope="col" class="px-6 py-3 md:block hidden">
+                                    Dept Descriptiono
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Action
+                                </th>
+                            </tr>            
+                        </thead>
+                        <tbody>
+        
+                        </tbody>
+                    </table>
+                </div>   
+        
+            </div>
+        
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default AllDepts
