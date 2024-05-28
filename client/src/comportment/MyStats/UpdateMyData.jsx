25d353b7519a173db371bd7d213e19a030287d1e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-28 12:22:07 +0530

    Update UpdateMyData.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/MyStats/UpdateMyData.jsx
+++ client/src/comportment/MyStats/UpdateMyData.jsx
@@ -9,14 +9,22 @@ const UpdateMyData = () => {
     //curent login user
     const RoleUser = secureLocalStorage.getItem("Login1");
     const EmailUser = secureLocalStorage.getItem("login2");
-    
-  return (
-    <div className='lg:text-2xl my-2 '>
-        <div className="bg-white py-4 px-6 rounded shadow-md">
-            <h1 className="text-gray-600">Update My Data</h1>
-        </div>
-    </div>
-  )
+
+    if(RoleUser !== null && EmailUser !== null){
+        return (
+            <div className='lg:text-2xl my-2 '>
+                <div className="bg-white py-4 px-6 rounded shadow-md">
+                    <h1 className="text-gray-600">Update My Data</h1>
+                </div>
+            </div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default UpdateMyData
