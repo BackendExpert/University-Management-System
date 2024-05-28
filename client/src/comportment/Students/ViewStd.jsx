8596d0562d7c822f85957a5a614f46c42b1046ec
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-28 11:19:21 +0530

    Update ViewStd.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Students/ViewStd.jsx
+++ client/src/comportment/Students/ViewStd.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 const ViewStd = () => {
-    const id = useParams(id)
+    const { id } = useParams()
   return (
     <div className='my-4 mx-8'>
         <div className="bg-white py-4 px-6 rounded shadow-md">
