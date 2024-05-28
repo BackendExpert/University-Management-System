79255e5eef5b7c98284d2df17df99de99844024e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-28 09:25:11 +0530

    Update AllDepts.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Departments/AllDepts.jsx
+++ client/src/comportment/Departments/AllDepts.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const AllDepts = () => {
   return (
-    <div>AllDepts</div>
+    <div className='bg-white py-4 px-8 rounded shadow-md'>
+        <h1 className="lg:text-3xl text-xl text-gray-500">Departments</h1>
+        <hr className='pb-4 pt-2'/>
+    </div>
   )
 }
 
