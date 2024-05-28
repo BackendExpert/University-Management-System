abd9bdc74cc4b86a52ee069ff1fa634ef82e376e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-28 09:38:10 +0530

    Update AllDepts.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Departments/AllDepts.jsx
+++ client/src/comportment/Departments/AllDepts.jsx
@@ -2,11 +2,34 @@ import React from 'react'
 
 const AllDepts = () => {
   return (
-    <div className='bg-white py-4 px-8 rounded shadow-md'>
-        <h1 className="lg:text-3xl text-xl text-gray-500">Departments</h1>
-        <hr className='pb-4 pt-2'/>
+    <div className="">
+        <div className='bg-white py-4 px-8 rounded shadow-md'>
+            <h1 className="lg:text-3xl text-xl text-gray-500">Departments</h1>
+            <hr className='pb-4 pt-2'/>
+        </div>
+
+        <table className='max-w-full'>
+            <thead>
+                <tr>
+                    <th className='font-semibold'>#</th>
+                    <th>Dept Name</th>
+                    <th>Dept Description</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td className='font-semibold'>1</td>
+                    <td>IT</td>
+                    <td>Information Tech</td>
+                    <th>Edit</th>
+                </tr>
+            </tbody>
+        </table>
+
     </div>
-  )
+
+    )
 }
 
 export default AllDepts
