c907995a6fc0e5c50dcfce50fe681e0b445d72ad
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-28 12:21:03 +0530

    Update UpdateMyData.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/MyStats/UpdateMyData.jsx
+++ client/src/comportment/MyStats/UpdateMyData.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup'
+import axios from 'axios';
 
 const UpdateMyData = () => {
+    const navigate = useNavigate()
+    //curent login user
+    const RoleUser = secureLocalStorage.getItem("Login1");
+    const EmailUser = secureLocalStorage.getItem("login2");
+    
   return (
     <div className='lg:text-2xl my-2 '>
         <div className="bg-white py-4 px-6 rounded shadow-md">
