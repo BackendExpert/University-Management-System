5911e3035f1e97c1e1b079bbb378b9e57441a54f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-28 11:25:08 +0530

    Update ViewStd.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Students/ViewStd.jsx
+++ client/src/comportment/Students/ViewStd.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 const ViewStd = () => {
     const { id } = useParams()
+
+    // when came to this page the page scroll to top
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    }, [])
+
   return (
     <div className='my-4 mx-8'>
         <div className="bg-white py-4 px-6 rounded shadow-md">
-            <h1 className="">Student Data : {id}</h1>
+            <div className="my-4">
+                <Link to={'../Students'}>
+                    <button className='bg-blue-500 py-2 px-4 rounded text-white shadow-md duration-500 hover:bg-blue-600'>Back</button>
+                </Link>
+            </div>
+            <h1 className="">Student Data : <span className="font-semibold">{id}</span></h1>
+            <hr className="py-2" />
         </div>
     </div>
   )
