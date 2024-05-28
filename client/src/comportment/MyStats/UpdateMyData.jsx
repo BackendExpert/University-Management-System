4d885862c58eceb19b23b5e1439e41f77107d244
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-28 12:26:59 +0530

    Update UpdateMyData.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/MyStats/UpdateMyData.jsx
+++ client/src/comportment/MyStats/UpdateMyData.jsx
@@ -15,6 +15,22 @@ const UpdateMyData = () => {
             <div className='lg:text-2xl my-2 '>
                 <div className="bg-white py-4 px-6 rounded shadow-md">
                     <h1 className="text-gray-600">Update My Data</h1>
+                    <hr />
+
+                    <form action="">
+                        <div className="md:grid grid-cols-2 gap-4">
+                            <div className="my-4">
+                                <label htmlFor="" className="text-gray-500">First Name : </label>
+                                <input type="text" name="" id="" className="my-2 h-12 w-full rounded bg-gray-200 pl-2" required placeholder='First Name'
+                                />
+                            </div>
+                            <div className="my-4">
+                                <label htmlFor="" className="text-gray-500">First Name : </label>
+                                <input type="text" name="" id="" className="my-2 h-12 w-full rounded bg-gray-200 pl-2" required placeholder='Last Name'
+                                />
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         )
