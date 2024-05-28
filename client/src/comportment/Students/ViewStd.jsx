1dea4de59daa4fee4a6a11682465f662589ba200
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-05-28 11:16:50 +0530

    Update ViewStd.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/comportment/Students/ViewStd.jsx
+++ client/src/comportment/Students/ViewStd.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const ViewStd = () => {
   return (
-    <div>ViewStd</div>
+    <div className='my-4 mx-8'>
+        <div className="bg-white py-4 px-6 rounded shadow-md">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta qui nulla asperiores delectus iure tempore quibusdam voluptate minima laboriosam. Soluta nemo quisquam molestias tenetur reiciendis nesciunt saepe itaque ad amet.
+        </div>
+    </div>
   )
 }
 
